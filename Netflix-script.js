e0af7a1a1dc64c2df6a814d9bbbae6bde7af7a17
Raj Mahajan <raj37mahajan@gmail.com>
2024-06-29 13:22:18 +0530

    UPDATE-3 29/06/2024

--- Netflix-script.js
+++ Netflix-script.js
@@ -1,7 +1,2 @@
 document.title = "Netflix India – Watch TV Shows Online, Watch Movies Online"
 document.body.style.fontFamily = "Trebuchet MS"
-
-document.querySelector(".start-button").addEventListener("click", ()=>{
-    alert("BSDK bohot jaldi ho rah h☠️☠️🤬")
-}
-)
